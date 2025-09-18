b923b27cc0ea3f60171107617d2e911ca7967a96
PEN <uemasaki20011222@gmail.com>
2025-09-18 13:24:01 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ruby on Rails 自動セットアップリポジトリ
+
+## Dockerの起動
+```bash
+cd docker
+docker compose up --build -d
+```
+
+## ブラウザを開く
+[Rails](http://localhost:3000/)
+
---
+++ docker/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+rm -f "/home/${USER_NAME}/${WS}/${APPNAME}/tmp/pids/server.pid"
+
+if [ ! -f "/home/${USER_NAME}/${WS}/${APPNAME}/Gemfile" ]; then
+    cd "/home/${USER_NAME}/"
+    
+    tee "Gemfile" <<EOF
+source 'https://rubygems.org'
+gem "rails",                      "~> 7.1.3"
+EOF
+
+    bundle install
+    bundle exec rails new "/home/${USER_NAME}/${WS}/${APPNAME}" --force
+fi
+
+cp "/home/${USER_NAME}/${WS}/config/Gemfile" "/home/${USER_NAME}/${WS}/${APPNAME}/Gemfile"
+
+cd "/home/${USER_NAME}/${WS}/${APPNAME}"
+
+bundle install
+bundle exec rails db:prepare
+exec bundle exec rails server -b 0.0.0.0
